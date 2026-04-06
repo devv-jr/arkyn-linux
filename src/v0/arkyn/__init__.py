5d41ebed9e08206676ee66354f07810740d54d85
Dev Jr <176184710+devv-jr@users.noreply.github.com>
2026-04-06 16:31:40 -0600

    Delete src/v0/arkyn directory

--- src/v0/arkyn/__init__.py
+++
@@ -1,2 +0,0 @@
-"""ARKYN package"""
-__version__ = "0.1.0"
--- src/v0/arkyn/plugins/example_plugin/plugin.py
+++
@@ -1,9 +0,0 @@
-"""Example plugin demonstrating the register contract"""
-
-def register(cli):
-    import click
-
-    @cli.command(name="hello-plugin")
-    def hello_plugin():
-        """Say hello (from plugin)"""
-        click.echo("Hello from example_plugin!")
